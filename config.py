43d395215c0d39c3f2e2a191912a64a0ae5833e3
maleon17 <andrey.mishin2008@gmail.com>
2026-02-05 20:59:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,21 @@
+BOT_TOKEN = "8214138180:AAFMNpZLq_srrSWwJBK-obXWF4ARBaleV_4"
 
+# Supergroup ID (где темы)
+DB_GROUP_ID = -1003745955855
+
+# topic IDs
+WHITELIST_TOPIC = 2
+BANLIST_TOPIC = 3
+PROFILES_TOPIC = 6
+
+# Admin TG IDs
+slastica = 2091726116
+maleon17 = 8480261623
+
+# Google Drive build link
+MODPACK_URL = "https://drive.google.com/..."
+
+# future
+RCON_HOST = ""
+RCON_PORT = 25575
+RCON_PASSWORD = ""
