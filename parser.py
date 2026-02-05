7e244ed559b29eafc3718d7b0967af2c8c18b46f
maleon17 <andrey.mishin2008@gmail.com>
2026-02-05 22:42:42 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -13,6 +13,8 @@ def save_db(data):
         json.dump(data, f, indent=2, ensure_ascii=False)
 
 
+# ───── USERS ─────
+
 def add_user(user):
     db = load_db()
     db["users"].append(user)
@@ -22,3 +24,34 @@ def add_user(user):
 def user_exists(tg_id):
     db = load_db()
     return any(u["telegram_id"] == tg_id for u in db["users"])
+
+
+def get_user(tg_id):
+    db = load_db()
+    for u in db["users"]:
+        if u["telegram_id"] == tg_id:
+            return u
+    return None
+
+
+# ───── BANS ─────
+
+def is_banned(tg_id):
+    db = load_db()
+    return any(b["telegram_id"] == tg_id for b in db["bans"])
+
+
+def ban_user(user):
+    db = load_db()
+
+    db["bans"].append(user)
+
+    db["users"] = [u for u in db["users"] if u["telegram_id"] != user["telegram_id"]]
+
+    save_db(db)
+
+
+def unban_user(tg_id):
+    db = load_db()
+    db["bans"] = [b for b in db["bans"] if b["telegram_id"] != tg_id]
+    save_db(db)
