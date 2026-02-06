6e5522e9f4d1f29e4fdfb68dc1cb8447584022c7
maleon17 <andrey.mishin2008@gmail.com>
2026-02-06 23:24:48 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,22 @@
+# future
+RCON_HOST = ""
+RCON_PORT = 25575
+RCON_PASSWORD = ""
+
+
 BOT_TOKEN = "8214138180:AAFMNpZLq_srrSWwJBK-obXWF4ARBaleV_4"
 
-# Supergroup ID (где темы)
-DB_GROUP_ID = -1003745955855
-
-# topic IDs
-WHITELIST_TOPIC = 2
-BANLIST_TOPIC = 3
-PROFILES_TOPIC = 6
-
-# Admin TG IDs
 ADMINS = [
     2091726116,
     8480261623
 ]
 
-# Google Drive build link
-MODPACK_URL = "https://drive.google.com/..."
+FACTIONS = ["🔴 Красные", "🔵 Синие"]
 
-# future
-RCON_HOST = ""
-RCON_PORT = 25575
-RCON_PASSWORD = ""
+KITS = [
+    "🪖 Воин",
+    "🎯 Снайпер",
+    "🛠 Инженер",
+    "🚁 Оператор БПЛА",
+    "👨‍⚕️ Медик"
+]
