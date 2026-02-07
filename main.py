5080ca0b2455e7cfaca52d39bab74176d9d76ca4
maleon17 <andrey.mishin2008@gmail.com>
2026-02-07 13:21:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import telebot
 from telebot import types
 from config import BOT_TOKEN, ADMINS, FACTIONS, KITS, MIRROR_GROUP
 import parser
-from parser import ban_user, unban_user, find_user, is_banned, add_user
+from parser import ban_user, unban_user, find_user, is_banned, add_user, mirror_load_db
 from logger import log
 from telebot.types import ReplyKeyboardRemove
 
 bot = telebot.TeleBot(BOT_TOKEN)
+mirror_load_db():
 
 sessions = {}
 
